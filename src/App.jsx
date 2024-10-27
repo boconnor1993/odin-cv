223ea7edc68cb283f969b2c5ce6640ff2f501dcc
boconnor1993 <brendan_oconnor1993@hotmail.com>
2024-10-27 17:10:19 +1000

    Added basic App structure and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <nav>
+        <h2>CV Generator</h2>
+      </nav>
+      <div className='container'>
+
+        <div className='input-container'>
+          <div className='general-info'>
+            <h2>General Information</h2>
+            <div className='general-info-container'>
+              
+            </div>
+          </div>
+
+          <div className='experience-info'>
+            <h2>Professional Experience</h2>
+            <div className='experience-info-container'></div>
+            <div className='button-container'>
+              <button>Add New</button>
+            </div>
+          </div>
+
+          <div className='education-info'>
+            <h2>Education</h2>
+            <div className='education-info-container'></div>
+            <div className='button-container'>
+              <button>Add New</button>
+            </div>
+          </div>      
+        </div>
+        <div className='output-container'>
+          <div className='cv-page'>
+
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
